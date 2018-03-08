91ff037c0d236b587e02eb6992ad2cc818c8c334
enuggetry <ericiam@berkeley.edu>
2018-03-08 00:01:05 +0000

    update

--- jblast-startup.js
+++ jblast-startup.js
@@ -4,16 +4,17 @@ const execa = require('execa');
 const sleep = require('system-sleep');
 const fs = require('fs-extra');
 
-fs.appendFileSync("/home/centos/jblast-startup/jblast-startup.log","starting... "+new Date().toISOString()+"\n");
+process.chdir('/home/ec2-user/jblast-startup'); 
+fs.appendFileSync("./jblast-startup.log","starting... "+new Date().toISOString()+"\n");
 
-process.chdir('/home/centos/galaxy'); 
+process.chdir('/home/ec2-user/galaxy'); 
 execa.shell('sh run.sh').then(result => {
     console.log(result.stdout);
 });
 
 sleep(10000);
 
-process.chdir('/home/centos/jbserver'); 
+process.chdir('/home/ec2-user/jbserver'); 
 execa.shell('sails lift > jblast-startup.log').then(result => {
     console.log(result.stdout);
 });
